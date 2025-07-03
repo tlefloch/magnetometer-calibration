0d5ec378e82c811050711e9ef66ce35ce94afdab
Tristan LE FLOCH <tristan.le_floch@ensta-bretagne.fr>
2025-07-03 09:46:25 +0200

    add time period selection in dataset

--- python/data_npz_to_txt.py
+++ python/data_npz_to_txt.py
@@ -2,6 +2,9 @@ import numpy as np
 
 data_path = "data/raw_data.npz"
 
+t0=0
+tf=-1  # Use -1 to load the entire dataset
+
 data = np.load(data_path)
 
 # Extracting all arrays
@@ -18,10 +21,24 @@ mag_z   = data["mag_z"]
 temp    = data["temp"]
 
 N=time.shape[0]
+i_t0 = np.argmin(np.abs(time - t0))
+
+if tf == -1:
+    i_tf = N-1
+else:
+    i_tf = np.argmin(np.abs(time - tf))
+
+t0= time[i_t0]
+tf = time[i_tf]
+
+print(f"t0: {t0} s")
+print(f"tf: {tf} s")
+print("i_t0:", i_t0)
+print("i_tf:", i_tf)
 
 mag_data_path="data/mag_data.txt"
 with open(mag_data_path,"w") as f:
-    for i in range(N):
+    for i in range(i_t0, i_tf+1):
         f.write(str(mag_x[i]))
         f.write(" ")
         f.write(str(mag_y[i]))
